eca62151096eee2b80af84074c59e8508c9875d4
Brett Lockhart <blockhart615@gmail.com>
2015-10-29 17:25:25 -0700

    Changed Mimi code to fit our Horse Race
    Game now creates 6 horses with random speeds and runs a race.
    having a problem where horses tie. Going to have to implement locks on
    the threads to avoid that.

--- JavaApplication4/src/RaceTester.java
+++ JavaApplication4/src/RaceTester.java
@@ -1,56 +1,50 @@
-/*Brett Lockhart
- *May 6, 2015
- *Purpose: create racers and simulate a race using threads
- *Inputs: number of racers, and name, speed, rest factor for each racer
- *Outputs: results of race */
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Random;
 
 public class RaceTester {
-	public static void main(String[] args) {
-		//declarations
-		Scanner input = new Scanner(System.in);
-		int numRacers = 0, speed, restFactor;
-		String name;
-		ArrayList<Thread> threadList = new ArrayList<Thread>();
-		ThreadGroup threads = new ThreadGroup("Racers");
-		
-		System.out.println("WELCOME TO THE RACE!");
-		System.out.println("The race consists of 4 laps, each of length 400m.");
-		
-		//determine number of racers
-		System.out.println("How many racers would you like? ");
-		numRacers = input.nextInt();
-		input.nextLine();
-		
-		//get Info about each racer
-		System.out.println("Each racer requires a name, speed, and rest factor.");		
-		for (int i = 1; i <= numRacers; i++) {
-			System.out.println("Enter info for racer #" + i);
-			
-			System.out.print("Name: ");
-			name = input.next();
-			System.out.print("Speed: ");
-			speed = input.nextInt();
-			System.out.print("Rest Factor: ");
-			restFactor = input.nextInt();
-			//create runnable object with speed and restFactor
-			Runnable r = new Racers(speed, restFactor);
-			//create thread, initialized into a threadGroup, and given the name
-			Thread t = new Thread(threads, r, name);
-			threadList.add(t);
-		}
-		//start the race!
-		for (Thread t: threadList)
-			t.start();
-		
-		//while racers are still active, wait.
-		while (threads.activeCount() != 0) {}
-		
-		//show the end results
-		System.out.println("\n\nRace Results: ");
-		for (Thread t: threadList)
-			System.out.println(t.getName() + " finished #" + t.getPriority() + ".");		
-		input.close();
-	}
+
+    public static void main(String[] args) {
+        //declarations
+        Scanner input = new Scanner(System.in);
+        int numRacers = 6, speed, restFactor;
+        Random rand = new Random();
+        String name;
+        ArrayList<Thread> threadList = new ArrayList<Thread>();
+        ThreadGroup threads = new ThreadGroup("Racers");
+
+        System.out.println("WELCOME TO THE RACE!");
+        System.out.println("The race consists of 4 laps, each of length 400m.");
+
+        //get Info about each racer
+        System.out.println("Each racer requires a name, speed, and rest factor.");
+        for (int i = 1; i <= numRacers; i++) {
+            
+            //randomly set speed for each horse and set rest factor
+            speed = rand.nextInt(100) + 50;
+            restFactor = 200;
+
+            //create runnable object with speed and restFactor
+            Runnable r = new Racers(speed, restFactor);
+
+            //create thread, initialized into a threadGroup, and given the name
+            Thread t = new Thread(threads, r, "Horse " + i);
+            threadList.add(t);
+        }
+        //start the race!
+        for (Thread t : threadList) {
+            t.start();
+        }
+
+        //while racers are still active, wait.
+        while (threads.activeCount() != 0) {
+        }
+
+        //show the end results
+        System.out.println("\n\nRace Results: ");
+        for (Thread t : threadList) {
+            System.out.println(t.getName() + " finished #" + t.getPriority() + ".");
+        }
+        input.close();
+    }
 }
--- JavaApplication4/src/Racers.java
+++ JavaApplication4/src/Racers.java
@@ -4,48 +4,48 @@
  *			the run method to display thread info as the race progresses
  *Inputs: Racer thread
  *Outputs: name, lap, current position, and placement of the racer */
-public class Racers implements Runnable{
-	private int speed, restFactor, distance, elapsedTime, laps;
-	private long startTime;
-	private final int RACE_LENGTH = 1600, LAP_DIST = 400;
-	static int place = 1;	//static variable to keep track of race results
-	
-	//initialize racers w/ a start speed and rest factor
-	public Racers(int iSpeed, int iRest) {
-		speed = iSpeed;
-		restFactor = iRest;
-	}
-	
-	//executes when the thread starts. simulates running a race
-	public void run() {
-		try {
-			startTime = System.currentTimeMillis() / 1000;
-			
-			while (laps < (RACE_LENGTH / LAP_DIST + 1) && !Thread.interrupted()) {
-				elapsedTime = (int)(System.currentTimeMillis() / 1000 - startTime);
-				distance = elapsedTime * speed;
-				laps = distance / LAP_DIST + 1; 
-				System.out.println(Thread.currentThread().getName() + ":\tLap# " + laps +
-				 "\tCurrent Distance = " + distance + "m.");
-				Thread.sleep(restFactor);
-			}
-		} 
-		catch (InterruptedException e) {
-			System.out.println("Interrupted.");
-		}	
-		//displays when racer finishes 
-		Thread.currentThread().setPriority(place);
-		System.out.println(Thread.currentThread().getName() + " FINISHED #"
-		 + Thread.currentThread().getPriority());
-		System.out.println(Thread.currentThread().getName() + " finished in " + elapsedTime + " seconds.");
-		++place;
-	}
+
+public class Racers implements Runnable {
+
+    private int speed, restFactor, distance, elapsedTime, laps;
+    private long startTime;
+    private final int RACE_LENGTH = 1000, LAP_DIST = 400;
+    static int place = 1;	//static variable to keep track of race results
+
+    //initialize racers w/ a start speed and rest factor
+    public Racers(int iSpeed, int iRest) {
+        speed = iSpeed;
+        restFactor = iRest;
+    }
+
+    //executes when the thread starts. simulates running a race
+    public void run() {
+        try {
+            //NEED TO IMPLEMENT LOCKS ON THE THREADS SO THAT THE HORSES DON'T TIE
+
+            startTime = System.currentTimeMillis() / 1000;
+            int count = 0;
+            while (laps < (RACE_LENGTH / LAP_DIST + 1) && !Thread.interrupted()) {
+                elapsedTime = (int) (System.currentTimeMillis() / 1000 - startTime);
+                distance = elapsedTime * speed;
+                laps = distance / LAP_DIST + 1;
+
+                if (count % 2 == 0) {
+                    System.out.println(Thread.currentThread().getName() + ":\tLap# " + laps
+                            + "\tCurrent Distance = " + distance + "m.");
+                }
+                
+                Thread.sleep(restFactor);
+                count++;
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted.");
+        }
+        //displays when racer finishes 
+        Thread.currentThread().setPriority(place);
+        System.out.println(Thread.currentThread().getName() + " FINISHED #"
+                + Thread.currentThread().getPriority());
+        System.out.println(Thread.currentThread().getName() + " finished in " + elapsedTime + " seconds.");
+        ++place;
+    }
 }
-/* When the rest factor is kept constant, and the speed is varied, the racers 
- * finished according to their speed. The racer with the highest speed finished
- * first, and the one with the lowest speed finished last.
- * 
- * When the speed was held constant, and the rest factor varied, the racers
- * with lower rest factors finished the race quicker than those with a high
- * rest factor. When the rest factor is increased, there is a longer delay 
- * between each time the racer displays its results.	*/
