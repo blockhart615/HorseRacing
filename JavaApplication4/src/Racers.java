31b6a249403474c27e3b3b500bdee3c5da7a1067
Brett Lockhart <blockhart615@gmail.com>
2015-11-10 17:16:06 -0800

    Added Locks so horses can't tie
    Yahoo. The horses can't tie anymore.

--- JavaApplication4/src/Racers.java
+++ JavaApplication4/src/Racers.java
@@ -1,12 +1,10 @@
-/*Brett Lockhart
- *May 6, 2015
- *Purpose: class contains information about the race, as well as
- *			the run method to display thread info as the race progresses
- *Inputs: Racer thread
- *Outputs: name, lap, current position, and placement of the racer */
+/*Brett Lockhart, Emil Evangelista, Dennis Lau
+ *Fall 2015
+ */
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Racers implements Runnable {
-
+    static ReentrantLock raceLock;
     private int speed, restFactor, distance, elapsedTime, laps;
     private long startTime;
     private final int RACE_LENGTH = 1000, LAP_DIST = 400;
@@ -22,7 +20,7 @@ public class Racers implements Runnable {
     public void run() {
         try {
             //NEED TO IMPLEMENT LOCKS ON THE THREADS SO THAT THE HORSES DON'T TIE
-
+            raceLock = new ReentrantLock();
             startTime = System.currentTimeMillis() / 1000;
             int count = 0;
             while (laps < (RACE_LENGTH / LAP_DIST + 1) && !Thread.interrupted()) {
@@ -30,7 +28,7 @@ public class Racers implements Runnable {
                 distance = elapsedTime * speed;
                 laps = distance / LAP_DIST + 1;
 
-                if (count % 2 == 0) {
+                if (count % 8 == 0) {
                     System.out.println(Thread.currentThread().getName() + ":\tLap# " + laps
                             + "\tCurrent Distance = " + distance + "m.");
                 }
@@ -41,11 +39,17 @@ public class Racers implements Runnable {
         } catch (InterruptedException e) {
             System.out.println("Interrupted.");
         }
-        //displays when racer finishes 
+        
+        
+        //When Racer Finishes
+        //set the racers place and display to the console
+        raceLock.lock();
         Thread.currentThread().setPriority(place);
+        
         System.out.println(Thread.currentThread().getName() + " FINISHED #"
                 + Thread.currentThread().getPriority());
         System.out.println(Thread.currentThread().getName() + " finished in " + elapsedTime + " seconds.");
         ++place;
+         raceLock.unlock();
     }
 }
